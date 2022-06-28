e38d8f4679323e5a954b32a583d999bc227bd981
mojtaba <kahroba013@yahoo.com>
2022-06-28 23:01:49 +0430

    Signed-off-by: mojtaba <kahroba013@yahoo.com>

--- src/js/cart.js
+++ src/js/cart.js
@@ -29,42 +29,47 @@ function showCartItems() {
   function createCartItems(cart) {
  
   const div=document.createElement('div');
- div.classList.add('bg-white','rounded-lg','items-stretch','shadow-lg','px-4','py-2','mx-4');
+ div.classList.add('bg-white','rounded-lg','shadow-lg','px-4','py-2','mx-4');
   div.innerHTML= `
-  <div class="grid grid-rows-4 grid-flow-col gap-x-4">
+  <div class="grid  grid-rows-4 grid-flow-col   gap-x-4">
 
-        <div class=" row-span-3   ">
-          <img class="w-full h-28" src=${cart.imageUrl} alt="">
+        <div class=" row-span-3  p-2  md:col-span-1  ">
+          <div class="flex items-center justify-center" >
+            <img class=" h-28 md:h-40 lg:h-48" src=${cart.imageUrl} alt="">
+          </div>          
         </div>
 
-        <div class=" row-span-1  flex items-center justify-center gap-x-2 ">
-          <span data-id=${cart.id} class="_plus cursor-pointer   flex font-medium items-center justify-center rounded-full bg-gray-200 w-6 h-6 md:w-7 md:h-7">+</span>
-          <span class="flex items-center justify-center border border-orange-500 rounded w-4 h-7 md:w-7 md:h-8 text-xs md:text-base" >${cart.quantity}</span>
-          <span data-id=${cart.id} class="_minus cursor-pointer flex font-medium items-center justify-center rounded-full bg-orange-200 w-6 h-6 md:w-7 md:h-7">-</span>
+        <div class=" row-span-1   ">
+          <div class="flex items-center justify-center gap-x-2 mt-1 md:mt-0">
+            <span data-id=${cart.id} class="_plus cursor-pointer   flex font-medium items-center justify-center rounded-full bg-gray-200 w-6 h-6 md:w-7 md:h-7">+</span>
+            <span class="flex items-center justify-center border border-orange-500 rounded w-4 h-7 md:w-7 md:h-8 text-xs md:text-base" >${cart.quantity}</span>
+            <span data-id=${cart.id} class="_minus cursor-pointer flex font-medium items-center justify-center rounded-full bg-orange-200 w-6 h-6 md:w-7 md:h-7">-</span>
+          </div>         
         </div>
 
         <div  class="col-span-3 row-span-4 py-2 ">
-        <section class="flex flex-col items-start justify-between">
-            <span class="text-sm font-semibold text-gray-700">${cart.title}</span>
-            <div class="flex items-center gap-x-2">
-            <span class="text-xs font-normal text-gray-500" >سری پردازنده:</span>
-            <span class="font-normal text-slate-800" >celeron</span>
+          <div class="flex flex-col justify-between py-4">
+            <section class="flex flex-col items-start justify-between mb-4">
+              <span class="text-sm font-semibold text-gray-700 mb-4 md:text-lg">${cart.title}</span>
+              <div class="flex items-center gap-x-2">
+              <span class="text-xs font-normal text-gray-500 md:text-base" >سری پردازنده:</span>
+              <span class="font-medium text-slate-800 " >celeron</span>
+              </div>
+              <div class="flex items-center gap-x-2">
+              <span class="text-xs font-base text-gray-500" >ظرفیت حافظه RAM :</span>
+              <span class="font-medium text-slate-800" >4 گیگ</span>
+              </div>
+              <div class="flex items-center gap-x-2">
+              <span class="text-xs font-base text-gray-500" >اندازه صفحه نمایش :</span>
+              <span class="font-medium text-slate-800" >15.6 اینچ</span>
+              </div>            
+              
+            </section>
+            <div class="flex items-center justify-between">
+              <span class="text-gray-700 text-sm font-bold md:text-lg">${cart.price} تومان</span>              
             </div>
-            <div class="flex items-center gap-x-2">
-            <span class="text-xs font-base text-gray-500" >ظرفیت حافظه RAM :</span>
-            <span class="font-normal text-slate-800" >4 گیگ</span>
-            </div>
-            <div class="flex items-center gap-x-2">
-            <span class="text-xs font-base text-gray-500" >اندازه صفحه نمایش :</span>
-            <span class="font-normal text-slate-800" >15.6 اینچ</span>
-            </div>
-            
-            <span class="text-gray-700 text-sm md:text-lg">${cart.price} تومان</span>
-        </section>
-          
-        </div>
-
-       
+          </div>
+        </div>       
        
       </div>
 `;
